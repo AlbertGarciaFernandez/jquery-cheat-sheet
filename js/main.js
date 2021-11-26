1c98e7e3b7d4785f5f6d4b91108e4831ee858dea
Albert Garcia F <alberd_beto@hotmail.com>
2021-11-26 10:12:16 +0100

    fix things

--- js/main.js
+++ js/main.js
@@ -1,10 +1,27 @@
-var elementschange = $('.functions').children("ul").children();
-console.log(elementschange);
+var functionschangeselect = $('.functions').children("ul").children();
+var selectorschangeselect = $('.selectors').children("ul").children();
+var eventschangeselect = $('.events').children("ul").children();
 
-elementschange.each(function(i, element){
+//case 0-18
+functionschangeselect.each(function(i, element){
     $(element).on("click",function(){
         vanillaJavaScript(i);
         jQueryScript(i);
     });
+});
 
-})
+//case 0-9
+selectorschangeselect.each(function(i, element){
+    $(element).on("click",function(){
+        vanillaJavaScript(i);
+        jQueryScript(i);
+    });
+});
+
+//case 0-12
+eventschangeselect.each(function(i, element){
+    $(element).on("click",function(){
+        vanillaJavaScript(i);
+        jQueryScript(i);
+    });
+});
